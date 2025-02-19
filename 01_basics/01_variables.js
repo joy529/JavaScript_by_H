1637070c45d5e487f199698583c7644647311255
joy jain <joyshah204@gmail.com>
2025-02-19 19:21:28 +0530

    learn variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,5 +1,38 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 
 
 
+// OUTPUT: 
+
+// ┌─────────┬─────────────┐
+// │ (index) │ Values      │
+// ├─────────┼─────────────┤
+// │ 0       │ 144553      │
+// │ 1       │ 'hc@hc.com' │
+// │ 2       │ '21212121'  │
+// │ 3       │ 'Bengaluru' │
+// │ 4       │ undefined   │
+// └─────────┴─────────────┘
 
